0eb297abe6db929ab87be7993fdbb2268a47b06f
LukasF7889 <lukas_fritsch@hotmail.de>
2025-05-09 09:44:32 +0200

    PlayerNames

--- src/components/Battlefield/PlayerOverview.jsx
+++ src/components/Battlefield/PlayerOverview.jsx
@@ -6,15 +6,16 @@ const PlayerOverview = ({ player, pokemon }) => {
   return (
     <div className="relative -top-8">
       <img
-        className="rounded-full shadow-2xl relative top-8 mx-auto"
+        className="rounded-full shadow-2xl relative top-8 mx-auto size-[65%]"
         src={player === 1 ? gP1 : gP2}
       ></img>
       <div className="bg-white rounded-lg p-4">
-        <div className="mt-8 grid grid-cols-2 w-full items-center gap-1">
+        <p className="mt-8 font-bold text-center">NAME</p>
+        <div className="grid grid-cols-2 w-full items-center gap-1">
           {pokemon?.map((e) => (
             <div key={e.id} className="">
               <img
-                className="w-[75px] mx-auto"
+                className="w-[55px] mx-auto"
                 src={e.sprites?.other?.[`official-artwork`].front_default}
                 alt={e.name}
               />
